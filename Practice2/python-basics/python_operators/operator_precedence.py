76066e2d545ec645ff9231df797a45aba514bf83
paradoxxk <halmurzaevislam625@gmail.com>
2026-02-06 10:53:54 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

--- Practice2/python-basics/python_operators/operator_precedence.py
+++ Practice2/python-basics/python_operators/operator_precedence.py
@@ -1,2 +1,4 @@
 #Parentheses has the highest precedence, meaning that expressions inside parentheses must be evaluated first:
 print((6 + 3) - (6 + 3))
+#Addition + and subtraction - has the same precedence, and therefore we evaluate the expression from left to right:
+print(5 + 4 - 7 + 3)
